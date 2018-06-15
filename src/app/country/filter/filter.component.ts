02a7785c831fbfc2e534e2acfd7ae0310447643e
SnehaPod <sneha@binarynumbers.io>
2018-06-15 19:39:28 +0530

    filter component started

---
+++ src/app/country/filter/filter.component.ts
@@ -0,0 +1,34 @@
+import { Component, OnInit } from '@angular/core';
+import { Location } from '@angular/common';
+import { ActivatedRoute , Router } from "@angular/router";
+import { CountriesApiService } from '../../countries-api.service'
+
+@Component({
+  selector: 'app-filter',
+  templateUrl: './filter.component.html',
+  styleUrls: ['./filter.component.css']
+})
+export class FilterComponent implements OnInit {
+
+  public info:any;
+  constructor(private location: Location, private _route:ActivatedRoute, private router:Router, private countryService: CountriesApiService) { }
+
+  ngOnInit() {
+    let lang = this._route.snapshot.paramMap.get('lang');
+
+    // pass regionName to api request function
+    this.countryService.fetchCountriesOfRegion(lang).subscribe(
+      data =>{
+        this.info = data;
+        console.log(this.info);
+      },
+      error =>{
+        console.log(error.errorMessage);
+      },
+    )
+  
+
+
+  }
+
+}
