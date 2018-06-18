5f3c697cbc05a241c303e52e6c9870a2261a96d2
unknown <akshaypatil@gmail.com>
2018-06-19 01:04:09 +0530

    Added new component

---
+++ src/app/shared/select-filter/select-filter.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit, OnChanges, Input, EventEmitter, Output } from '@angular/core';
+
+@Component({
+  selector: 'app-select-filter',
+  templateUrl: './select-filter.component.html',
+  styleUrls: ['./select-filter.component.css']
+})
+export class SelectFilterComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,10 +1,25 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { SelectFilterComponent } from './select-filter/select-filter.component';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {  MatInputModule, MatCardModule, MatFormFieldModule, MatSelectModule, MatOptionModule, MatTooltipModule } from '@angular/material';
+
 
 @NgModule({
   imports: [
     CommonModule
   ],
-  declarations: []
+  declarations: [SelectFilterComponent],
+  exports:[
+    CommonModule,
+    FormsModule, 
+    ReactiveFormsModule,
+    BrowserAnimationsModule,
+    MatFormFieldModule, 
+    MatSelectModule, 
+    MatOptionModule,
+    MatTooltipModule
+  ]
 })
 export class SharedModule { }
