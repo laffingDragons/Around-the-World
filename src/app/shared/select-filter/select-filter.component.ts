ce02a31f103f8b2f7e5d656592ca74810bd883f1
laffingragons <akshaypatil,vit@gmail.com>
2018-06-19 17:24:42 +0530

    added shared module

--- src/app/shared/select-filter/select-filter.component.ts
+++ src/app/shared/select-filter/select-filter.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit, OnChanges, Input, EventEmitter, Output } from '@angular/core';
+import { Router } from "@angular/router";
+import { CountriesApiService } from '../../countries-api.service'
+
 
 @Component({
   selector: 'app-select-filter',
@@ -7,9 +10,57 @@ import { Component, OnInit, OnChanges, Input, EventEmitter, Output } from '@angu
 })
 export class SelectFilterComponent implements OnInit {
 
-  constructor() { }
+  public selectedValueLang : string;
+  public tempArrayForLang : any;
+  public langCode = [];
+  public uniqueLangCode :any;
 
-  ngOnInit() {
+  public selectedValueCurrency : string;
+  public tempArrayForCurrency : any;
+  public currencyCode = [];
+  public uniqueCurrencyCode :any;
+
+  constructor( public router:Router, private countryService: CountriesApiService) {}
+  
+  ngOnInit(){   
+
+      this.countryService.fetchAll().subscribe(
+        data=>{
+          this.tempArrayForLang = data;
+          // console.log(this.tempArray);
+          this.langIteration();
+          this.currencyIteration();
+        }
+      ); //get request ends here
+    }
+  
+
+    langIteration(){
+
+      for(let i of this.tempArrayForLang){
+        for(let j of i.languages){
+          this.langCode.push(j.iso639_1);
+        }
+      }
+       this.uniqueLangCode = Array.from(new Set(this.langCode))
+      //  console.log("!!!!!", this.tempArrayForLang);
+     }
+
+     currencyIteration(){
+      for(let m of this.tempArrayForLang){
+        for(let n of m.currencies){
+           this.currencyCode.push(n.code);
+          }
+        }
+        this.uniqueCurrencyCode = Array.from(new Set(this.currencyCode))
+     }
+
+  fetchCountriesForLang(event){
+    this.router.navigate(['/filter', this.selectedValueLang]);
+    
   }
 
+  fetchCountriesForCurrency(event){
+    this.router.navigate(['/filter', this.selectedValueCurrency]);
+  }
 }
